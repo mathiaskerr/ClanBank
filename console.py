0c33436a21314cb56f492fdf55104c8d1f9d1ee8
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-02-02 21:52:55 +0000

    x

---
+++ console.py
@@ -0,0 +1,14 @@
+import pdb
+from models.merchant import merchant
+from models.purchase import purchase
+from models.tag import tag
+from models.user import user 
+
+import repositories.merchant_repository as merchant_repository
+import repositories.purchase_repository as purchase_repository
+import repositories.tag_repository as tag_repository
+import repositories.user_repository as user_repository
+
+
+
+pdb.set_trace()
