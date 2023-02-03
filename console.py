098311b47a26f8efd848acbaddb42d945e73aca3
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-02-03 11:21:27 +0000

    file structure

--- console.py
+++ console.py
@@ -1,13 +1,9 @@
 import pdb
-from models.merchant import merchant
-from models.purchase import purchase
-from models.tag import tag
-from models.user import user 
+from models.merchant import Merchant
+from models.transaction import Transaction
 
 import repositories.merchant_repository as merchant_repository
-import repositories.purchase_repository as purchase_repository
-import repositories.tag_repository as tag_repository
-import repositories.user_repository as user_repository
+import repositories.transaction_repository as transaction_repository
 
 
 
--- repositories/user_repository.py
+++
@@ -1,1 +0,0 @@
-from db.run_sql import run_sql
