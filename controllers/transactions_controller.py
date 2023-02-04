753db34bfab2780209d7497be8e2521222d495c8
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-02-04 12:27:48 +0000

    view indivivdual transactions complete

--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -1,3 +1,4 @@
+import pdb
 from flask import Flask, render_template, request, redirect
 from repositories import transaction_repository
 from repositories import merchant_repository
@@ -5,14 +6,31 @@ from models.transaction import Transaction
 
 from flask import Blueprint
 
+
 transactions_blueprint = Blueprint("transactions",__name__)
 
 @transactions_blueprint.route("/transactions")
 def transactions():
     transactions = transaction_repository.select_all()
-    return render_template("transactions/index.html", all_transactions = transactions)
+    return render_template("transactions/index.html", all_transactions = transactions) 
 
 @transactions_blueprint.route("/transactions/new")
 def new_transaction():
     merchants = merchant_repository.select_all()
-    return render_template("transactions/new.html", all_merchants = merchants)   
+    return render_template("transactions/new.html", all_merchants = merchants)
+
+@transactions_blueprint.route("/transactions", methods = ["POST"])
+def create_transaction():
+    merchant_id = request.form['merchant_id']
+    tag = request.form['tag']
+    time = request.form['time']
+    amount = request.form['amount']
+    merchant = merchant_repository.select(merchant_id)
+    transaction = Transaction(merchant, tag, amount, time)
+    transaction_repository.save(transaction)
+    return redirect('/transactions')
+
+@transactions_blueprint.route("/transaction/<id>")
+def show_transaction(id):
+    transaction = transaction_repository.select(id)
+    return render_template("transactions/show.html", show_transaction = transaction)
