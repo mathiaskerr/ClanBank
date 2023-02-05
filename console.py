d954f0bff22e4f4efdec5fd5bb4e033746745a6d
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-02-05 17:15:29 +0000

    austin

--- console.py
+++ console.py
@@ -9,13 +9,13 @@ import repositories.user_repository as user_repository
 
 merchant_repository.delete_all()
 transaction_repository.delete_all()
-# user_repository.delete_all()
+user_repository.delete_all()
 
-merchant_1 = Merchant('Lidl', "grocery")
-merchant_2 = Merchant('Tesco', 'Grocery')
-merchant_3 = Merchant('Howlin Wolf', 'Bar')
+merchant_1 = Merchant('UK Government', "Espionage")
+merchant_2 = Merchant('Jaguar', 'Automobile')
+merchant_3 = Merchant('Love Honey', '****')
 
-user_1 = User("Mathias", "Kerr", 1000.00)
+user_1 = User("Austin", "Powers", 1000.00)
 result = user_repository.save(user_1)
 
 
@@ -26,9 +26,9 @@ merchant_repository.save(merchant_3)
 # result = merchant_repository.select_all()
 # merchant_repository.delete(19)
 
-transaction_1 = Transaction(merchant_1, 'Weekly Shop', 50.50, '14:15')
-transaction_2 = Transaction(merchant_2, 'Alcohol', 35.50, '12:00')
-transaction_3 = Transaction(merchant_3, 'Food' , 14.00, '21:50')
+transaction_1 = Transaction(merchant_1, 'Guns', 45000, '14:15')
+transaction_2 = Transaction(merchant_2, 'Shaguar', 60000, '12:00')
+transaction_3 = Transaction(merchant_3, 'Love Honey' , 69.00, '21:50')
 # transaction_repository.delete(40)
 transaction_repository.save(transaction_1)
 transaction_repository.save(transaction_2)
